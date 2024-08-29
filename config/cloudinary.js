ae12ef76cad28be595879dd2384f2354a07d0643
garimanagdeve1218 <garimanagdeve210463@acropolis.in>
2024-08-29 10:27:05 +0530

    server branch

--- config/cloudinary.js
+++ config/cloudinary.js
@@ -5,7 +5,7 @@ exports.cloudnairyconnect= ()=>{
         cloudinary.config({
             cloud_name : process.env.CLOUD_NAME,
             api_key : process.env.API_KEY,
-            api_secret : process.env.API_SECRET
+                             api_secret : process.env.API_SECRET
         })
         console.log("CD connected");
 
